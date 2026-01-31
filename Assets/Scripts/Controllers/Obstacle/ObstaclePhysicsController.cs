ead4fc946e0299fe5ba8d751eca69feb85a750c8
OnurHG <onurhangumus@gmail.com>
2026-01-31 11:52:49 +0300

    -Obstable breaking mechanic has been added -bullet time visual effects were improved -bullet time icon has been newed

--- Assets/Scripts/Controllers/Obstacle/ObstaclePhysicsController.cs
+++ Assets/Scripts/Controllers/Obstacle/ObstaclePhysicsController.cs
@@ -8,11 +8,15 @@ public class ObstaclePhysicsController : MonoBehaviour, IReplaceable
 {
     #region Self Variables
     #region Inject Variables
+    [Inject] private PoolSignals _poolSignals { get; set; }
     #endregion
     #region Public Variables
     #endregion
     #region Serializefield Variables
     [SerializeField] private Rigidbody rig;
+    [SerializeField] private int health = 2;
+    [SerializeField] private bool isUnbreakable = false;
+    private int _takenDamage = 0;
     #endregion
     #region Private Variables
 
@@ -25,5 +29,15 @@ public class ObstaclePhysicsController : MonoBehaviour, IReplaceable
     public void OnShooted(Vector3 velocity)
     {
         rig.linearVelocity = rig.linearVelocity + velocity * 0.8f ;
+        if (isUnbreakable)
+        {
+            return;
+        }
+        ++_takenDamage;
+        if (_takenDamage >= health)
+        {
+            _poolSignals.onGetObject(Enums.PoolEnums.ObstacleExplosion, transform.position).SetActive(true);
+            transform.parent.gameObject.SetActive(false);
+        }
     }
 }
